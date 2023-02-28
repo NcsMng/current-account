6bd02ac8b3b24bd06e70111782c3d5f1f7437c2d
NcsMng <ncs.gabriel.w@gmail.com>
2023-02-28 17:42:59 +0100

    Added customers to H2 on project startup and enabled logs on project (debug)

--- src/main/java/com/ncsgab/currentaccount/CurrentAccountApplication.java
+++ src/main/java/com/ncsgab/currentaccount/CurrentAccountApplication.java
@@ -1,15 +1,41 @@
 package com.ncsgab.currentaccount;
 
+import com.ncsgab.currentaccount.dto.request.NewAccountRequest;
+import com.ncsgab.currentaccount.dto.response.AccountDto;
+import com.ncsgab.currentaccount.model.Customer;
+import com.ncsgab.currentaccount.repository.CustomerRepository;
+import com.ncsgab.currentaccount.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 @EnableJpaAuditing
-public class CurrentAccountApplication {
+@Slf4j
+public class CurrentAccountApplication implements CommandLineRunner {
+    private final CustomerRepository customerRepository;
+    private final AccountService accountService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrentAccountApplication.class, args);
-	}
+    public CurrentAccountApplication(CustomerRepository customerRepository, AccountService accountService) {
+        this.customerRepository = customerRepository;
+        this.accountService = accountService;
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(CurrentAccountApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        Customer customer = customerRepository.save(new Customer("Mario", "Rossi", null));
+        Customer customer2 = customerRepository.save(new Customer("Luigi", "Verdi", null));
+        NewAccountRequest newAccountRequest = new NewAccountRequest(1L, new BigDecimal(1000));
+        AccountDto account = accountService.createAccount(newAccountRequest);
+        log.debug("Created customer 1 :{} \n With account: {}", customer, account);
+        log.debug("Created customer 2 :{}", customer2);
+    }
 }
