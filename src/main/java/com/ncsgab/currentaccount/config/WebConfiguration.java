52588935ef178b68e5dd12df8c44ba59647a7d6a
NcsMng <ncs.gabriel.w@gmail.com>
2023-03-02 14:00:12 +0100

    Moved WebConfiguration in configs Upgraded log4j version to 2.20

--- src/main/java/com/ncsgab/currentaccount/config/WebConfiguration.java
+++ src/main/java/com/ncsgab/currentaccount/config/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.ncsgab.currentaccount;
+package com.ncsgab.currentaccount.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
